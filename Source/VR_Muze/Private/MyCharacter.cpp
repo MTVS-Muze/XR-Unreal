8181d14a8724a63430b1202caa8e5b01ec5ff429
Semira5 <semira5@naver.com>
2023-10-23 09:26:39 +0900

    1020

--- Source/VR_Muze/Private/MyCharacter.cpp
+++ Source/VR_Muze/Private/MyCharacter.cpp
@@ -24,10 +24,7 @@ void AMyCharacter::BeginPlay()
 
 	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
 	{
-// 		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
-// 		{
-// 			Subsystem->AddMappingContext(DefaultMappingContext, 0);
-// 		}
+
 		if (PlayerController != nullptr)
 		{
 			PlayerController->bShowMouseCursor = true;
